1535e3a82a9b9726edd2d7754b027f9491719c6c
Keith Cirkel <keithamus@users.noreply.github.com>
2020-03-12 16:27:22 +0000

    docs: document all exported functions

--- catalyst/src/controller.ts
+++ catalyst/src/controller.ts
@@ -6,6 +6,12 @@ interface CustomElement {
   new(): HTMLElement
 }
 
+/**
+ * Controller is a decorator to be used over a class that extends HTMLElement.
+ * It will automatically `register()` the component in the customElement
+ * registry, as well as ensuring `bind(this)` is called on `connectedCallback`,
+ * wrapping the classes `connectedCallback` method if needed.
+ */
 export function controller(classObject: CustomElement) {
   wrap(classObject, 'connectedCallback', bind)
   register(classObject)
--- catalyst/src/target.ts
+++ catalyst/src/target.ts
@@ -1,5 +1,12 @@
 import {findTarget, findTargets} from './findtarget'
 
+/**
+ * Target is a decorator which - when assigned to a property field on the
+ * class - will override that class field, turning it into a Getter which
+ * returns a call to `findTarget(this, key)` where `key` is the name of the
+ * property field. In other words, `@target foo` becomes a getter for
+ * `findTarget(this, 'foo')`.
+ */
 export function target(proto: object, key: string) {
   Object.defineProperty(
     proto,
@@ -11,6 +18,13 @@ export function target(proto: object, key: string) {
   );
 }
 
+/**
+ * Targets is a decorator which - when assigned to a property field on the
+ * class - will override that class field, turning it into a Getter which
+ * returns a call to `findTargets(this, key)` where `key` is the name of the
+ * property field. In other words, `@targets foo` becomes a getter for
+ * `findTargets(this, 'foo')`.
+ */
 export function targets(proto: object, key: string) {
   Object.defineProperty(
     proto,
