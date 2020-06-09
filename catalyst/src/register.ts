d8245cfbe1619e0105aec02df4fc41e024372ba2
Kristján Oddsson <koddsson@gmail.com>
2020-06-09 14:37:27 +0100

    ignore impossible error
    Co-authored-by: Keith Cirkel <keithamus@users.noreply.github.com>

--- catalyst/src/register.ts
+++ catalyst/src/register.ts
@@ -12,6 +12,8 @@ interface CustomElement {
 export function register(classObject: CustomElement): void {
   const name = classObject.name.replace(/([a-zA-Z])(?=[A-Z])/g, '$1-').toLowerCase()
   if (!window.customElements.get(name)) {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
     window[classObject.name] = classObject
     window.customElements.define(name, classObject)
   }
