487986ff176d969607e1cc1f087abefbdaa30a8d
Kristján Oddsson <koddsson@gmail.com>
2020-06-11 10:08:35 +0100

    Register a specific custom element for find target tests

--- catalyst/test/findtarget.js
+++ catalyst/test/findtarget.js
@@ -1,14 +1,9 @@
 import {findTarget, findTargets} from '../lib/findtarget.js'
 
-class MyController extends HTMLElement {}
-if (!window.customElements.get('my-controller')) {
-  window.MyController = MyController
-  window.customElements.define('my-controller', MyController)
-}
-
 describe('findTarget', () => {
-  let root
+  window.customElements.define('find-target-test-controller', class extends HTMLElement {})
 
+  let root
   beforeEach(() => {
     root = document.createElement('div')
     document.body.appendChild(root)
@@ -19,31 +14,33 @@ describe('findTarget', () => {
   })
 
   it('calls querySelectorAll with the controller name and target name', () => {
-    const instance = new MyController()
+    const instance = document.createElement('find-target-test-controller')
     chai.spy.on(instance, 'querySelectorAll', () => [])
     findTarget(instance, 'foo')
-    expect(instance.querySelectorAll).to.have.been.called.once.with.exactly('[data-target~="my-controller.foo"]')
+    expect(instance.querySelectorAll).to.have.been.called.once.with.exactly(
+      '[data-target~="find-target-test-controller.foo"]'
+    )
   })
 
   it('returns the first element where closest tag is the controller', () => {
     const els = [document.createElement('div'), document.createElement('div')]
-    const instance = new MyController()
+    const instance = document.createElement('find-target-test-controller')
     chai.spy.on(instance, 'querySelectorAll', () => els)
     chai.spy.on(els[0], 'closest', () => null)
     chai.spy.on(els[1], 'closest', () => instance)
     const target = findTarget(instance, 'foo')
-    expect(els[0].closest).to.have.been.called.once.with.exactly('my-controller')
-    expect(els[1].closest).to.have.been.called.once.with.exactly('my-controller')
+    expect(els[0].closest).to.have.been.called.once.with.exactly('find-target-test-controller')
+    expect(els[1].closest).to.have.been.called.once.with.exactly('find-target-test-controller')
     expect(target).to.equal(els[1])
   })
 
   it('returns the first element that has the exact target name', () => {
-    const instance = document.createElement('my-controller')
+    const instance = document.createElement('find-target-test-controller')
 
     const notExactMatch = document.createElement('div')
-    notExactMatch.setAttribute('data-target', 'my-controller.foobar')
+    notExactMatch.setAttribute('data-target', 'find-target-test-controller.foobar')
     const exactMatch = document.createElement('div')
-    exactMatch.setAttribute('data-target', 'my-controller.foo')
+    exactMatch.setAttribute('data-target', 'find-target-test-controller.foo')
 
     instance.appendChild(notExactMatch)
     instance.appendChild(exactMatch)
@@ -54,10 +51,10 @@ describe('findTarget', () => {
   })
 
   it('returns targets when there are mutliple target values', () => {
-    const instance = document.createElement('my-controller')
+    const instance = document.createElement('find-target-test-controller')
 
     const el = document.createElement('div')
-    el.setAttribute('data-target', 'my-controller.barfoo my-controller.foobar')
+    el.setAttribute('data-target', 'find-target-test-controller.barfoo find-target-test-controller.foobar')
 
     instance.appendChild(el)
 
@@ -69,10 +66,10 @@ describe('findTarget', () => {
   })
 
   it('returns targets when there are mutliple target values with different controllers', () => {
-    const instance = document.createElement('my-controller')
+    const instance = document.createElement('find-target-test-controller')
 
     const el = document.createElement('div')
-    el.setAttribute('data-target', 'other-controller.barfoo my-controller.foobar')
+    el.setAttribute('data-target', 'other-controller.barfoo find-target-test-controller.foobar')
 
     instance.appendChild(el)
 
@@ -86,23 +83,25 @@ describe('findTarget', () => {
 
 describe('findTargets', () => {
   it('calls querySelectorAll with the controller name and target name', () => {
-    const instance = new MyController()
+    const instance = document.createElement('find-target-test-controller')
     chai.spy.on(instance, 'querySelectorAll', () => [])
     findTargets(instance, 'foo')
-    expect(instance.querySelectorAll).to.have.been.called.once.with.exactly('[data-target~="my-controller.foo"]')
+    expect(instance.querySelectorAll).to.have.been.called.once.with.exactly(
+      '[data-target~="find-target-test-controller.foo"]'
+    )
   })
 
   it('returns all elements where closest tag is the controller', () => {
     const els = [document.createElement('div'), document.createElement('div'), document.createElement('div')]
-    const instance = new MyController()
+    const instance = document.createElement('find-target-test-controller')
     chai.spy.on(instance, 'querySelectorAll', () => els)
     chai.spy.on(els[0], 'closest', () => instance)
     chai.spy.on(els[1], 'closest', () => null)
     chai.spy.on(els[2], 'closest', () => instance)
     const targets = findTargets(instance, 'foo')
-    expect(els[0].closest).to.have.been.called.once.with.exactly('my-controller')
-    expect(els[1].closest).to.have.been.called.once.with.exactly('my-controller')
-    expect(els[2].closest).to.have.been.called.once.with.exactly('my-controller')
+    expect(els[0].closest).to.have.been.called.once.with.exactly('find-target-test-controller')
+    expect(els[1].closest).to.have.been.called.once.with.exactly('find-target-test-controller')
+    expect(els[2].closest).to.have.been.called.once.with.exactly('find-target-test-controller')
     expect(targets).to.deep.equal([els[0], els[2]])
   })
 })
