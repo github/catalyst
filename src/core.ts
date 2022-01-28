ffe94382fe89e273f40f8e38cdf589184a8a0c9d
Keith Cirkel <keithamus@users.noreply.github.com>
2022-01-28 17:57:24 +0000

    Refactor controller contents into `src/core`
    This keeps `controller` as a simple decorator.

---
+++ src/core.ts
@@ -0,0 +1,26 @@
+import {register} from './register.js'
+import {bind, bindShadow} from './bind.js'
+import {autoShadowRoot} from './auto-shadow-root.js'
+import {defineObservedAttributes, initializeAttrs} from './attr.js'
+import type {CustomElement} from './custom-element.js'
+
+const instances = new WeakSet<Element>()
+
+export function initializeInstance(instance: HTMLElement, connect?: (this: HTMLElement) => void): void {
+  instance.toggleAttribute('data-catalyst', true)
+  instances.add(instance)
+  autoShadowRoot(instance)
+  initializeAttrs(instance)
+  bind(instance)
+  if (connect) connect.call(instance)
+  if (instance.shadowRoot) bindShadow(instance.shadowRoot)
+}
+
+export function initializeClass(classObject: CustomElement): void {
+  defineObservedAttributes(classObject)
+  register(classObject)
+}
+
+export function initialized(el: Element): boolean {
+  return instances.has(el)
+}
