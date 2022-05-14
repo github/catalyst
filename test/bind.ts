b9961322456b6fba116d4a9da09b8b1bcd142c94
Keith Cirkel <keithamus@users.noreply.github.com>
2022-05-14 10:20:51 +0100

    Update bind.ts

--- test/bind.ts
+++ test/bind.ts
@@ -1,5 +1,5 @@
 import {expect, fixture, html} from '@open-wc/testing'
-import {replace, fake} from 'sinon'
+import {fake} from 'sinon'
 import {controller} from '../src/controller.js'
 import {bindShadow} from '../src/bind.js'
 
