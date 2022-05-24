3f824fe9f8108595cc61605cd1b1b44e22c2e3de
Keith Cirkel <keithamus@users.noreply.github.com>
2022-05-24 10:24:57 +0100

    rename parse to parseActionAttribute

--- src/actionable.ts
+++ src/actionable.ts
@@ -4,12 +4,12 @@ import {register, add, tags} from './tag-observer.js'
 import {controllable, attachShadowCallback} from './controllable.js'
 import {createAbility} from './ability.js'
 
-const parse = (tag: string): [tagName: string, event: string, method: string] => {
+const parseActionAttribute = (tag: string): [tagName: string, event: string, method: string] => {
   const eventSep = tag.lastIndexOf(':')
   const methodSep = Math.max(0, tag.lastIndexOf('#')) || tag.length
   return [tag.slice(eventSep + 1, methodSep), tag.slice(0, eventSep), tag.slice(methodSep + 1) || 'handleEvent']
 }
-register('data-action', parse, (el: Element, controller: Element | ShadowRoot, tag: string, event: string) => {
+register('data-action', parseActionAttribute, (el: Element, controller: Element | ShadowRoot, tag: string, event: string) => {
   el.addEventListener(event, handleEvent)
 })
 
@@ -17,7 +17,7 @@ const actionables = new WeakSet<CustomElement>()
 // Bind a single function to all events to avoid anonymous closure performance penalty.
 function handleEvent(event: Event) {
   const el = event.currentTarget as Element
-  for (const [tag, type, method] of tags(el, 'data-action', parse)) {
+  for (const [tag, type, method] of tags(el, 'data-action', parseActionAttribute)) {
     if (event.type === type) {
       type EventDispatcher = CustomElement & Record<string, (ev: Event) => unknown>
       const controller = el.closest<EventDispatcher>(tag)!
