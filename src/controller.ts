d79fc59ac769e56d6b0d22c3d2bebdde10cca5ee
Keith Cirkel <keithamus@users.noreply.github.com>
2022-05-26 10:22:28 +0100

    use targetable in controller

--- src/controller.ts
+++ src/controller.ts
@@ -1,7 +1,8 @@
-import {CatalystDelegate} from './core.js'
 import type {CustomElementClass} from './custom-element.js'
+import {targetable} from './targetable.js'
 import {attrable} from './attrable.js'
 import {actionable} from './actionable.js'
+import {register} from './register.js'
 
 /**
  * Controller is a decorator to be used over a class that extends HTMLElement.
@@ -9,6 +10,6 @@ import {actionable} from './actionable.js'
  * registry, as well as ensuring `bind(this)` is called on `connectedCallback`,
  * wrapping the classes `connectedCallback` method if needed.
  */
-export function controller(classObject: CustomElementClass): void {
-  new CatalystDelegate(actionable(attrable(classObject)))
+export function controller<T extends CustomElementClass>(Class: T) {
+  return register(actionable(attrable(targetable(Class))))
 }
